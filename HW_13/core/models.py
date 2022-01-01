7f5afd7f42369d10db162f52356579da64edaa38
unoplus <unoplus6t@gmail.com>
2022-01-01 12:58:07 -0800

    new sections in to admin panel

--- HW_13/core/models.py
+++ HW_13/core/models.py
@@ -1,29 +1,25 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.deletion import CASCADE
-from django.db.models.expressions import F
 
 
-class SetTimeData(models.Model):
+class SetDefaultFields(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODULE, null=False, blank=False, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
 
 
-class Post(SetTimeData):
+class Post(SetDefaultFields):
     title = models.CharField(max_length=100, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     images = models.CharField(max_length=255, null=True, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODULE, null=True,
-                                blank=False, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
 
 
-class Like(SetTimeData):
+class Like(SetDefaultFields):
     post = models.ForeignKey('Post', null=False, blank=False, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODULE, null=False, blank=False, on_delete=models.CASCADE)
     status = models.BooleanField(null=True, blank=True)
