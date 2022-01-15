326785981b88bae35295a64e94bfc058cefb81d0
unoplus <unoplus6t@gmail.com>
2022-01-14 17:39:37 -0800

    create some new features in to amin panel

--- HW_13/core/admin.py
+++ HW_13/core/admin.py
@@ -1,15 +1,19 @@
+from re import search
 from django.contrib import admin
-from django.db.models.base import Model
 from .models import Like, Post
 
 
 class LikeAdmin(admin.TabularInline):
     model = Like
 
-
-
-@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created_at', 'updated_at')
-    ordering = ('created_at',)
-    inlines = [LikeAdmin,]
+    search_fields = ('title', )
+    list_filter = ('user', 'updated_at')
+    ordering = ('-updated_at',)
+    inlines = [
+        LikeAdmin,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
