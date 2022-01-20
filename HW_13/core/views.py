9728335488e061e9f74cf094240edd6f1f95ba23
unoplus <unoplus6t@gmail.com>
2022-01-19 16:16:39 -0800

    adding new functional class ViewPost

--- HW_13/core/views.py
+++ HW_13/core/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
 from .models import Post
 
 
@@ -8,11 +9,33 @@ class PostsView(ListView):
     context_object_name = 'posts'
     ordering = ('-updated_at',)
 
+   
     def get_queryset(self):
-        posts_queryset = super().get_queryset()
-        for post in posts_queryset:
+        posts_queryset = super().get_queryset().select_related('user')
+        for post in posts_queryset:          
             post.likes = post.like_set.filter(status=True).count()
             post.dislikes = post.like_set.filter(status=False).count()
             post.count = Post.objects.filter(user_id=post.user.pk).count()
-           
-        return posts_queryset   
+        return posts_queryset
+
+
+class ViewPost(DetailView):
+    model = Post
+    template_name = 'core/view_post.html'
+    context_object_name = 'views'
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        view = Post.objects.get(pk=pk)
+        view.likes = view.like_set.filter(status=True).count()
+        view.dislikes = view.like_set.filter(status=False).count()
+        view.post_count = Post.objects.filter(user_id=view.user.pk).count()
+        return view
+
+
+class DeletePost(DeleteView):
+    model = Post
+    template_name = 'core/delete_post.html'
+    context_object_name = 'delete'
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('all_posts')
