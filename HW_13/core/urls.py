77402d89b62dfbc2a7ac007b132da00bb36ddd6e
unoplus <unoplus6t@gmail.com>
2022-01-24 00:48:11 -0800

    adding new routes

--- HW_13/core/urls.py
+++ HW_13/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PostsView, ViewPost, DeletePost
+from .views import PostsView, UpdatePost, ViewPost, DeletePost, CreatePost
 
 
 urlpatterns = [
     path('', PostsView.as_view(), name='all_posts'),
     path("post/<int:pk>/", ViewPost.as_view(), name="post_view"),
-    path("delete_post/<int:pk>/", DeletePost.as_view(), name="delete"),
+    path("delete_post/<int:pk>/", DeletePost.as_view(), name="delete_post"),
+    path("create_post/", CreatePost.as_view(), name="create_post"),
+    path("update_post/<int:pk>/", UpdatePost.as_view(), name="update_post"),
 ]
 
